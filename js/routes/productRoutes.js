691032c9a302affb5f9a3b8d3c71cfe8c9b0d08f
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2023-12-07 09:43:29 +0200

    Add example of middleware usage

--- js/routes/productRoutes.js
+++ js/routes/productRoutes.js
@@ -4,10 +4,26 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const router = new express.Router();
 
+// const middleware1 = (req, res, next) => {
+//   console.log('Hello from middleware 1');
+//   next();
+// };
+// const middleware2 = (req, res, next) => {
+//   console.log('Hello from middleware 2\nRoute: ' + req.originalUrl);
+//   next();
+// };
+
+// router.use(middleware1);
+
 router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
-router.patch('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+
+// router.use(middleware2);
+
+router
+  .route('/:id')
+  .get(productController.getProductById)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 module.exports = router;
